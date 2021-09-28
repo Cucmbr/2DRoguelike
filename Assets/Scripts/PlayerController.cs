e4e993a790ceabbd1fead1e9a6cc6febb1a12ebf
YanKonopelko <komand47@mail.ru>
2021-09-28 23:04:11 +0300

    Передвижение персонажа с поворотом в нужную сторону.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,13 +4,26 @@ public class PlayerController : MonoBehaviour
 {
     [SerializeField] public Rigidbody2D _rigidbody;
     [SerializeField] public FixedJoystick _joystick;
-
+    public bool flipRot = true;
     public float _moveSpeed;
 
     private void FixedUpdate()
     {
-        //_rigidbody.velocity = new Vector3(_joystick.Horizontal * _moveSpeed, _rigidbody.velocity.y, _joystick.Vertical * _moveSpeed);
-        //transform.rotation = Quaternion.LookRotation(_rigidbody.velocity);
-        transform.position += new Vector3(_joystick.Horizontal * _moveSpeed, _joystick.Vertical * _moveSpeed,0); 
+
+
+
+
+        
+        if (_joystick.Horizontal != 0 || _joystick.Vertical != 0)
+         {
+        float horizontal = _joystick.Direction.x;
+        float vertical = _joystick.Direction.y;
+
+        float angle = Mathf.Atan2(horizontal, vertical) * Mathf.Rad2Deg;
+        angle = flipRot ? -angle : angle;
+        transform.rotation = Quaternion.Euler(new Vector3(0, 0, angle));
+         }
+
+        transform.position += new Vector3(_joystick.Horizontal * _moveSpeed, _joystick.Vertical * _moveSpeed, 0);
     }
 }
