0f75b3ce5215e1e150b671963739762d41258bc4
YanKonopelko <komand47@mail.ru>
2021-09-28 21:00:37 +0300

    Сделал движение персонажа

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] public Rigidbody2D _rigidbody;
+    [SerializeField] public FixedJoystick _joystick;
+
+    public float _moveSpeed;
+
+    private void FixedUpdate()
+    {
+        //_rigidbody.velocity = new Vector3(_joystick.Horizontal * _moveSpeed, _rigidbody.velocity.y, _joystick.Vertical * _moveSpeed);
+        //transform.rotation = Quaternion.LookRotation(_rigidbody.velocity);
+        transform.position += new Vector3(_joystick.Horizontal * _moveSpeed, _joystick.Vertical * _moveSpeed,0); 
+    }
+}
