9ed762805c7bf2a6b37e99eb7e91941652ac9d51
Cucmbr <vanvanich531@gmail.com>
2021-10-03 13:05:13 +0200

    Imp теперь с запуска сцены движется в направлении игрока. Также, если получилось так, что Imp движется почти по прямой, то ему задается новое случайное направление

--- Assets/Scripts/ImpMovement.cs
+++ Assets/Scripts/ImpMovement.cs
@@ -6,22 +6,21 @@ public class ImpMovement : MonoBehaviour
     public float _moveSpeed;
     public bool flipRot = true;
     public Transform rotationTarget;
+    public Transform player;
 
     private void Start()
     {
-        //Задаются случайные координаты направления движения и присваиваются _direction
-        float x = Random.Range(-1f, 1f);
-        int sign = Random.Range(0, 1);
-        if (sign == 0)
-            sign = -1;
-        float y = sign * (1 - Mathf.Abs(x));
-        _direction = new Vector2(x, y);
-    }
+        //Высчитывается положение Player и задается направление движения Imp'а в его направлении
+        Vector3 targetPos = player.position;
 
-    private void FixedUpdate()
-    {
-        //Движение
-        transform.Translate(_direction * _moveSpeed);
+        targetPos.z = 0f;
+        targetPos.x = targetPos.x - transform.position.x;
+        targetPos.y = targetPos.y - transform.position.y;
+
+        float x = targetPos.x / (Mathf.Abs(targetPos.x) + Mathf.Abs(targetPos.y));
+        float y = targetPos.y / (Mathf.Abs(targetPos.x) + Mathf.Abs(targetPos.y));
+
+        _direction = new Vector2(x, y);
 
         //Высчитывание угла для поворота спрайта по направлению движения самого объекта
         float angle = Mathf.Atan2(_direction.x, _direction.y) * Mathf.Rad2Deg;
@@ -29,14 +28,56 @@ public class ImpMovement : MonoBehaviour
         rotationTarget.rotation = Quaternion.Euler(new Vector3(0, 0, angle + 180));
     }
 
+    private void FixedUpdate()
+    {
+        //Движение
+        transform.Translate(_direction * _moveSpeed);
+
+        
+    }
+
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.CompareTag("Wall"))
         {
-            //Отскок от стен под тем же углом
-            Vector2 inDirection = _direction;
-            Vector2 inNormal = collision.contacts[0].normal;
-            _direction = Vector2.Reflect(inDirection, inNormal);
+            //Если направление почти по прямой то задаются случайные значения направления
+            if (Mathf.Abs(_direction.x) > 0.9f || Mathf.Abs(_direction.y) > 0.9f)
+            {
+                if (Mathf.Abs(_direction.x) > 0.9f)
+                {
+                    float x = _direction.x < 0 ? Random.Range(0, 1f) : Random.Range(-1f, 0);
+                    int sign = Random.Range(0, 1);
+                    if (sign == 0)
+                        sign = -1;
+                    float y = sign * (1 - Mathf.Abs(x));
+                    _direction = new Vector2(x, y);
+                }
+                if (Mathf.Abs(_direction.y) > 0.9f)
+                {
+                    float y = _direction.y < 0 ? Random.Range(0, 1f) : Random.Range(-1f, 0);
+                    int sign = Random.Range(0, 1);
+                    if (sign == 0)
+                        sign = -1;
+                    float x = sign * (1 - Mathf.Abs(y));
+                    _direction = new Vector2(x, y);
+                }
+
+                //Высчитывание угла для поворота спрайта по направлению движения самого объекта
+                float angle = Mathf.Atan2(_direction.x, _direction.y) * Mathf.Rad2Deg;
+                angle = flipRot ? -angle : angle;
+                rotationTarget.rotation = Quaternion.Euler(new Vector3(0, 0, angle + 180));
+            }
+            //Отскок от стены под тем же углом
+            else
+            {
+                Vector2 inDirection = _direction;
+                Vector2 inNormal = collision.contacts[0].normal;
+                _direction = Vector2.Reflect(inDirection, inNormal);
+                //Высчитывание угла для поворота спрайта по направлению движения самого объекта
+                float angle = Mathf.Atan2(_direction.x, _direction.y) * Mathf.Rad2Deg;
+                angle = flipRot ? -angle : angle;
+                rotationTarget.rotation = Quaternion.Euler(new Vector3(0, 0, angle + 180));
+            }
         }
     }
 }
